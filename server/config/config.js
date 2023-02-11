f35f15ffd09a19f57e2e65301523cf75afc4611e
Justin Cheema <justincheema@quirklogic.com>
2023-02-10 21:24:28 -0700

    task_5 - Task 5 complete. Put in basic cache for the beer request made to Punk API.

--- server/config/config.js
+++ server/config/config.js
@@ -3,5 +3,8 @@ module.exports = {
 
   beerDataUrl: 'https://api.punkapi.com/', // Url that will be used to obtain beer data
 
-  dbLocation: './TestDatabase' // Location of the database
+  dbLocation: './TestDatabase', // Location of the database
+
+  cacheTTL: 600, // Number of seconds before the cache is flushed
+  checkPeriod: 60 // Number of seconds used for the automatic delete check interval
 }
