17db3663485655414a1b8512949b4ebd2b192de6
Justin Cheema <justincheema@quirklogic.com>
2023-02-10 17:27:23 -0700

    task_2 - Completed work for task 2.

--- server/lib/model/db_document.js
+++ server/lib/model/db_document.js
@@ -1,6 +1,9 @@
 const { randomUUID } = require('crypto')
 const { Database } = require('./../database')
 
+/**
+ * Generic DbDocument. Child classes can extend this class to create whatever kind of documents they wish.
+ */
 class DbDocument {
   constructor (prefix, type, id) {
     if (!id) {
@@ -13,6 +16,11 @@ class DbDocument {
     this.modified = timestamp
   }
 
+  /**
+   * Insert this dbDocument into the database. According to the documents this insert function is supposed
+   * to return a Database Builder which has a callback which is called when the operation is complete. On
+   * testing however no Database Builder object is returned! Proceeding with an asynchronous insert for now.
+   */
   insert () {
     Database.insert(this)
   }
